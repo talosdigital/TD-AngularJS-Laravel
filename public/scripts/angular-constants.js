bf86b64d459624d0d42dec47dbd660c294000895
Mateo Carvajal <mateocarvajal@MacBook-Pro-Mateo-212.local>
2016-01-07 13:57:35 -0500

    style changes

--- public/scripts/angular-constants.js
+++ public/scripts/angular-constants.js
@@ -3,7 +3,7 @@
 
 angular.module('constants', [])
 
-.constant('commit', '1f81646897ccba5e5c266b6856f5ac7b7308e7c9')
+.constant('commit', 'cad8264edea8cf7fd4e2a0cc30261bb51e6ada4f')
 
 ;
 })(angular);
